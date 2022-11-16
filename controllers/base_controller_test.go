f5d51e06cb4ef8edcfa19cb81d9eff6d8625f373
Matt Wise <matt@nextdoor.com>
2022-11-16 14:49:58 -0800

    tidy

---
+++ controllers/base_controller_test.go
@@ -0,0 +1,152 @@
+package controllers
+
+import (
+	"context"
+	"fmt"
+
+	api "github.com/diranged/oz/api/v1alpha1"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+var _ = Describe("OzReconciler Tests", Ordered, func() {
+	Context("Method Tests", func() {
+		const TestName = "base-controller-test"
+		var namespace *corev1.Namespace
+
+		// Logger for our tests - makes it easier for us to debug sometimes
+		ctx := context.Background()
+		logger := log.FromContext(ctx)
+
+		BeforeAll(func() {
+			By("Creating the Namespace to perform the tests")
+			namespace = &corev1.Namespace{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      TestName,
+					Namespace: TestName,
+				},
+			}
+			err := k8sClient.Create(ctx, namespace)
+			Expect(err).To(Not(HaveOccurred()))
+		})
+
+		It("Refetch() should work", func() {
+			// Initial test configmap
+			cm := &corev1.ConfigMap{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      TestName,
+					Namespace: TestName,
+				},
+				Data: map[string]string{
+					"foo.data": "test data",
+				},
+			}
+
+			// Base OzReconciler
+			reconciler := &OzReconciler{
+				Client:    k8sClient,
+				Scheme:    k8sClient.Scheme(),
+				ApiReader: k8sClient,
+			}
+
+			By("Creating a ConfigMap to reference for tests")
+			// Create the config map and gather its resource version
+			err := k8sClient.Create(ctx, cm)
+			Expect(err).To(Not(HaveOccurred()))
+			origResourceVer := cm.ResourceVersion
+			logger.V(1).Info(fmt.Sprintf("Original ConfigMap ResourceVersion: %s", origResourceVer))
+
+			// Now update the configmap...
+			cm.Data = map[string]string{
+				"foo.new": "test data",
+			}
+			err = k8sClient.Update(ctx, cm)
+			Expect(err).To(Not(HaveOccurred()))
+			newResourceVer := cm.ResourceVersion
+			logger.V(1).Info(fmt.Sprintf("Updated ConfigMap ResourceVersion: %s", newResourceVer))
+
+			// Verify that the two numbers do not match..
+			Expect(origResourceVer).To(Not(Equal(newResourceVer)))
+
+			// Now, refetch th data
+			reconciler.Refetch(ctx, cm)
+
+			// Verify that the new object has the new resource version
+			Expect(newResourceVer).To(Equal(cm.ResourceVersion))
+
+		})
+
+		It("UpdateStatus() should work", func() {
+			originalReq := &api.AccessRequest{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      TestName,
+					Namespace: TestName,
+				},
+				Spec: api.AccessRequestSpec{
+					TemplateName: "Junk",
+					Duration:     "1h",
+				},
+			}
+
+			reconciler := &OzReconciler{
+				Client:    k8sClient,
+				Scheme:    k8sClient.Scheme(),
+				ApiReader: k8sClient,
+			}
+
+			By("Creating an AccessRequest resource to update")
+			err := k8sClient.Create(ctx, originalReq)
+			Expect(err).To(Not(HaveOccurred()))
+
+			By("Verifying the initial PodName is empty")
+			Expect(originalReq.Status.PodName).To(Equal(""))
+
+			By("Set the Status.PodName to something")
+			originalReq.Status.PodName = "bogus"
+			err = reconciler.UpdateStatus(ctx, originalReq)
+			Expect(err).To(Not(HaveOccurred()))
+
+			By("Get a new reference to the AccessRequest, verify the PodName status")
+			freshReq := &api.AccessRequest{}
+			err = k8sClient.Get(ctx, types.NamespacedName{
+				Name:      TestName,
+				Namespace: TestName,
+			}, freshReq)
+			Expect(err).To(Not(HaveOccurred()))
+			Expect(freshReq.Status.PodName).To(Equal("bogus"))
+		})
+
+		It("UpdateStatus() should return failures properly", func() {
+			request := &api.AccessRequest{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      fmt.Sprintf("%s-fail-test", TestName),
+					Namespace: TestName,
+				},
+				Spec: api.AccessRequestSpec{
+					TemplateName: "Junk",
+					Duration:     "1h",
+				},
+			}
+
+			reconciler := &OzReconciler{
+				Client:    client.NewNamespacedClient(k8sClient, "bogus"),
+				Scheme:    k8sClient.Scheme(),
+				ApiReader: client.NewNamespacedClient(k8sClient, "bogus"),
+			}
+
+			By("Creating an AccessRequest resource to update")
+			err := k8sClient.Create(ctx, request)
+			Expect(err).To(Not(HaveOccurred()))
+
+			By("Now try to update the request with a narrowly scoped namespaced client")
+			err = reconciler.UpdateStatus(ctx, request)
+			Expect(err).To(HaveOccurred())
+		})
+	})
+
+})
--- go.mod
+++ go.mod
@@ -4,9 +4,11 @@ go 1.19
 
 require (
 	github.com/go-logr/logr v1.2.3
+	github.com/jsternberg/zap-logfmt v1.3.0
 	github.com/onsi/ginkgo/v2 v2.5.0
 	github.com/onsi/gomega v1.24.1
 	github.com/spf13/cobra v1.6.1
+	go.uber.org/zap v1.23.0
 	k8s.io/api v0.25.4
 	k8s.io/apimachinery v0.25.4
 	k8s.io/cli-runtime v0.25.4
@@ -29,6 +31,7 @@ require (
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/jsonreference v0.19.5 // indirect
 	github.com/go-openapi/swag v0.19.14 // indirect
+	github.com/go-task/slim-sprig v0.0.0-20210107165309-348f09dbbbc0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
@@ -43,7 +46,6 @@ require (
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/jsternberg/zap-logfmt v1.3.0 // indirect
 	github.com/liggitt/tabwriter v0.0.0-20181228230101-89fcab3d43de // indirect
 	github.com/mailru/easyjson v0.7.6 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
@@ -51,6 +53,8 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/monochromegane/go-gitignore v0.0.0-20200626010858-205db1a8cc00 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/nxadm/tail v1.4.8 // indirect
+	github.com/onsi/ginkgo v1.16.5 // indirect
 	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/prometheus/client_golang v1.12.2 // indirect
@@ -63,17 +67,18 @@ require (
 	go.starlark.net v0.0.0-20200306205701-8dd3e2ee1dd5 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
-	go.uber.org/zap v1.23.0 // indirect
 	golang.org/x/net v0.2.0 // indirect
 	golang.org/x/oauth2 v0.0.0-20221014153046-6fdb5e3db783 // indirect
 	golang.org/x/sys v0.2.0 // indirect
 	golang.org/x/term v0.2.0 // indirect
 	golang.org/x/text v0.4.0 // indirect
 	golang.org/x/time v0.0.0-20220609170525-579cf78fd858 // indirect
+	golang.org/x/tools v0.2.0 // indirect
 	gomodules.xyz/jsonpatch/v2 v2.2.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/apiextensions-apiserver v0.25.0 // indirect
