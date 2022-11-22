c974c7eae95ac8a03b5db27b56952f75daacfc46
Matt Wise <768067+diranged@users.noreply.github.com>
2022-11-21 17:30:47 -0800

    bug: fix the creation of the k8s client in the cli (#10)

--- ozctl/cmd/root.go
+++ ozctl/cmd/root.go
@@ -9,24 +9,15 @@ import (
 	"github.com/spf13/cobra"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 	"k8s.io/client-go/kubernetes/scheme"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 // Variables filled out during the initial `Execute()` step. These are used throughout the various
 // commands create within this CLI tool.
 var (
-	// A populated controller-runtime.client.Client object that can be used to make REST calls to Kubernetes.
-	KubeClient client.Client
-
-	// Shortcut to the supplied "--namespace" flag value (if supplied), or the default value from
-	// the .kubeconfig. Finally defaults to "default" if nothing else is supplied.
-	KubeNamespace = "default"
-
-	//
-	Username = os.Getenv("USER")
+	Username        = os.Getenv("USER")
+	kubeConfigFlags = genericclioptions.NewConfigFlags(true)
 )
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "ozctl",
 	Short: "Manage Oz Access Requests and Approvals",
@@ -38,19 +29,8 @@ var rootCmd = &cobra.Command{
 func Execute() {
 	verifyUsernameSet()
 
-	// Set up common CLI flags, and then build the Kubernetes Client Object from them
-	configFlags := genericclioptions.NewConfigFlags(true)
-	configFlags.AddFlags(rootCmd.PersistentFlags())
-
-	// Build the Kubernetes client
-	kubeRestCfg, _ := configFlags.ToRESTConfig()
-	KubeClient, _ = client.New(kubeRestCfg, client.Options{})
-
 	// Make sure to add in our api schemes with the custom resources
 	api.AddToScheme(scheme.Scheme)
-
-	// Determine the default namespace
-	KubeNamespace = getDefaultKubeNamespace(configFlags)
 
 	// Set up the root command and make sure that doesn't fail.
 	if err := rootCmd.Execute(); err != nil {
@@ -59,21 +39,25 @@ func Execute() {
 	}
 }
 
-func getDefaultKubeNamespace(cf *genericclioptions.ConfigFlags) string {
-	clientConfig := cf.ToRawKubeConfigLoader()
-	discoveredNamespace, _, err := clientConfig.Namespace()
-	if err != nil {
-		return "default"
-	}
-	return discoveredNamespace
-}
-
 func verifyUsernameSet() error {
 	if Username == "" {
 		fmt.Println("ERROR: This CLI tool requires that the $USER environment be set to something")
 		os.Exit(1)
 	}
 	return nil
+}
+
+func getDefaultKubeNamespace(cf *genericclioptions.ConfigFlags) string {
+	if *cf.Namespace != "" {
+		return *cf.Namespace
+	}
+
+	clientConfig := cf.ToRawKubeConfigLoader()
+	discoveredNamespace, _, err := clientConfig.Namespace()
+	if err != nil {
+		return "default"
+	}
+	return discoveredNamespace
 }
 
 func init() {
