695e6fda4096b21e9f2577feb63af18b9380294b
Matt Wise <matt@nextdoor.com>
2022-11-21 20:30:41 -0800

    lint

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// Package v1alpha1 contains API Schema definitions for the templates v1alpha1 API group
 // +kubebuilder:object:generate=true
 // +groupName=crds.wizardofoz.co
 package v1alpha1
