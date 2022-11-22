0142e100ec304c27130ab6baa4c31ca52ffc5c19
Matt Wise <matt@nextdoor.com>
2022-11-21 20:06:26 -0800

    autogen docs

--- Custom.mk
+++ Custom.mk
@@ -4,6 +4,9 @@ SOURCE := $(wildcard api/*/*.go controller/*.go ozctl/*.go ozctl/*/*.go)
 REVIVE_VER ?= v1.2.4
 REVIVE     ?= $(LOCALBIN)/revive
 
+GEN_CRD_API_DOCS_VER ?= v0.3.1-0.20220223025230-af7c5e0048a3
+GEN_CRD_API_DOCS     ?= $(LOCALBIN)/go-crd-api-reference-docs
+
 .PHONY: docker-load
 docker-load:
 	kind load docker-image $(IMG) -n $(KIND_CLUSTER_NAME)
@@ -20,11 +23,23 @@ lint: revive
 test-e2e:
 	go test ./test/e2e/ -v -ginkgo.v
 
-
 .PHONY: revive
 revive: $(REVIVE) ## Download revive locally if necessary.
 $(REVIVE): $(LOCALBIN) Custom.mk
 	GOBIN=$(LOCALBIN) go install github.com/mgechev/revive@$(REVIVE_VER)
+
+gen-crd-api-reference-docs: $(GEN_CRD_API_DOCS)
+$(GEN_CRD_API_DOCS):
+	GOBIN=$(LOCALBIN) go install github.com/ahmetb/gen-crd-api-reference-docs@$(GEN_CRD_API_DOCS_VER)
+
+.PHONY: godocs
+godocs: $(GEN_CRD_API_DOCS)
+	bin/gen-crd-api-reference-docs \
+		-config $$(go env GOPATH)/src/github.com/diranged/oz/gen-crd-api-reference-docs.json \
+		-api-dir ./api/v1alpha1 \
+		-template-dir $$(go env GOMODCACHE)/github.com/ahmetb/gen-crd-api-reference-docs@$(GEN_CRD_API_DOCS_VER)/template \
+		-out-file docs.md \
+		-v 5
 
 ##@ Build CLI
 .PHONY: cli
